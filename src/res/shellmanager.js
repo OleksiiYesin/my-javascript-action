b557a40908a95d63abbd07d7cf3fc47d30a9a562
Oleskii Yesin <oleskii@gmail.com>
2022-06-19 14:49:31 +0000

    commit

--- src/res/shellmanager.js
+++ src/res/shellmanager.js
@@ -1,52 +1,50 @@
 const shell = require('@actions/exec');
-const { exec } = require('child_process');
 
-exec('cd', function (err, stdout, stderr){
-    if (err) console.error(stderr);
-    console.log(stdout);
-})
-
-// const shell = require('@actions/exec');
-
-// class ShellManager {
-//     constructor() {
-//         this.payload = '';
-//     }
+class ShellManager {
+    constructor() {
+        this.payload = '';
+    }
 
 
-//   async run (cmd, args, opt) {
+  async run (cmd, args, opt) {
 
-//     if (!opt) opt = {};
+    this.clean();
 
-//     if (!Object.hasOwnProperty('listeners'));
-//       opt.listeners = {
-//         stdout: (buffer) => this.appendOut(buffer),
-//         stderr: (buffer) => this.appendErr(buffer)
-//       }
+    if (!opt) opt = {};
+
+    if (!Object.hasOwnProperty('listeners'));
+      opt.listeners = {
+        stdout: (buffer) => this.appendOut(buffer),
+        stderr: (buffer) => this.appendErr(buffer)
+      }
     
-//       await this.exec(cmd, args, opt);
+      await this.exec(cmd, args, opt);
 
-//       if (this.payload) return this.payload.trim();
-//   }
+      if (this.payload) return this.payload.trim();
+  }
 
-//   async exec(cmd, args, opt) {
-//     try {
-//         await shell.exec(cmd, args, opt);
-//     }
-//     catch(error) {
-//         console.error('\nError running command ' + cmd + ' ' + args.join(' '));
-//         console.error('Ýou can probably find more details below\n');
-//         throw error;
-//     }
-//   }
+  async exec(cmd, args, opt) {
+    try {
+        await shell.exec(cmd, args, opt);
+    }
+    catch(error) {
+        console.error('\nError running command ' + cmd + ' ' + args.join(' '));
+        console.error('Ýou can probably find more details below\n');
+        throw error;
+    }
+  }
 
-//   appendOut(b) {
-//     this.payload += b;
-//   }
+  appendOut(b) {
+    this.payload += b;
+  }
 
-//   appendErr(b) {
-//     this.payload = '';
-//   }
-// }
+  appendErr(b) {
+    this.payload += b;
+  }
+
+  clean() {
+    this.payload = '';
+  }
+}
 
 module.exports = ShellManager;
