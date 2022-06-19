1fe6b5dff3d8d25964056ee1d22431b3dac62a19
Oleskii Yesin <oleskii@gmail.com>
2022-06-19 11:54:02 +0000

    commit

--- src/res/nuget.js
+++ src/res/nuget.js
@@ -42,7 +42,7 @@ class Nuget {
 
   async hello() {
     try {
-        let name = await this.getInput('who');
+        let name = await this.inputs('who');
         this.info(`Hello ${name}`)
     }
     catch(e) {
